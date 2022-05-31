20aa7a332e5e37606bcb490b566a8c9b8fcde9cc
elene638 <elenejavaxishvili@yahoo.com>
2022-05-30 20:53:19 -0700

    Footer edit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ import React from "react";
 
 function Footer () {
     return (
-        <footer>
-            <h1>Footer</h1>
-            <i className="fa-brands fa-facebook-square"></i>
-            <i className="fa-brands fa-instagram-square"></i>
-            <i className="fa-brands fa-linkedin"></i>
-            <i className="fa-brands fa-github-square"></i>
-            <i className="fa-brands fa-twitter-square"></i>
+        <footer className="footer" >
+            {/* <h1>Footer</h1> */}
+            <i className="fa-brands fa-facebook-square fa-2x"></i>
+            <i className="fa-brands fa-instagram-square fa-2x"></i>
+            <i className="fa-brands fa-linkedin fa-2x"></i>
+            <i className="fa-brands fa-github-square fa-2x"></i>
+            <i className="fa-brands fa-twitter-square fa-2x"></i>
         </footer>
     )
 }
